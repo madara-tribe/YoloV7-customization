9a48db9e26c64df67971ee95ea9604cb2e7b6a68
madara-tribe <tsukuba.univer@gmail.com>
2022-11-08 22:15:52 +0900

    as prototype

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -1,5 +1,5 @@
 #/bin/sh
-mkdir -p up/Docker up/cfg up/customs up/data up/deploy up/models up/sh up/utils
+mkdir -p up/Docker up/cfg up/customs up/data up/deploy up/models up/scripts up/utils
 cp *.py *.txt up/
 cp -r Docker/* up/Docker/
 cp -r cfg/* up/cfg/
@@ -7,6 +7,6 @@ cp -r customs/*.py up/customs/
 cp -r data/*.yaml up/data/
 cp -r deploy/* up/deploy/
 cp -r models/*.py up/models/
-cp -r sh/*.sh up/sh/
+cp -r scripts/*.sh up/scripts/
 cp -r utils/*.py utils/wandb_logging up/utils/
  
